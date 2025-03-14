92badb4c8d792033ce0f53b54856fc2a6d612c88
Firaaaaaaaaaaa <Shafira.fadiyah1000@gmail.com>
2025-03-14 08:49:17 +0700

    menyesuaikan nama menu recap

--- apps/myuser/urls.py
+++ apps/myuser/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('rekapitulasi_keseluruhan/', views.rekapitulasi_keseluruhan, name='rekapitulasi-keseluruhan'),
     path('rekapitulasi_pribadi/', views.rekapitulasi_pribadi, name='rekapitulasi-pribadi'), 
     path('profile/', views.profile_user, name='profile-user'), 
+    path("update-profile/<int:user_id>/", views.update_profile, name="update_profile"),
+    path('change-password/', views.change_password, name='change-password'),
 ]
